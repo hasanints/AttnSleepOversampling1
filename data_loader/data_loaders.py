45659a915fe6bdfcc5cfab2459125d1a81385eb1
hasanints <18320020@std.stei.itb.ac.id>
2025-01-24 14:17:32 +0700

    update

--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -36,7 +36,7 @@ def apply_adasyn_1_2(X_train, y_train):
     print(f"Distribusi kelas sebelum ADASYN: {class_counts}")
     
     class_2_count = class_counts[2]
-    sampling_strategy = {1: class_2_count // 2} 
+    sampling_strategy = {3: class_2_count // 2} 
 
     adasyn = ADASYN(sampling_strategy=sampling_strategy, random_state=42)
 
